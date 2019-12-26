09514402c57bf49ec312b419cd58f3bc3fe4d951
SaidulHaq <58678551+SaidulHaq@users.noreply.github.com>
2019-12-26 14:27:33 +0800

    Add files via upload

---
+++ expenditures.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM expand.expenditures
+LIMIT 0, 1000
+
+-- Date: 2019-12-26 14:26
+*/
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (1,10.5,'2019-12-18','bills',1);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (1,12.5,'2019-12-18','groceries',2);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (2580,20.5,'2019-12-25','bills',3);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (2580,2.5,'2019-12-25','other',5);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (1,25,'2019-12-03','bills',6);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (1,50,'2019-12-04','bills',7);
+INSERT INTO `` (`ic`,`amount`,`ex_date`,`ex_type`,`ex_id`) VALUES (1,50,'2019-11-25','bills',8);
---
+++ login.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM expand.login
+LIMIT 0, 1000
+
+-- Date: 2019-12-26 14:26
+*/
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('','','');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('1','sadi','sadi');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('2580','hafizul','0000');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('333','hadi','hadi');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('77777','yello','789');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('99999','jhon','jhon');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('555','fahmi','fahmi');
+INSERT INTO `` (`c_ic`,`username`,`password`) VALUES ('bc123456','abdul','111');
